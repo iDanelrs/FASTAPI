581b8a209d310b9e837a0fe4f8448a23906aa142
Danel Rolon <danelrolon10@gmail.com>
2025-01-30 17:37:06 -0600

    ai

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
-app= FastAPI()
+app= FastAPI(
+    title="Mi primer FastAPI",
+    description="Alfredo Danel Rolón Salazar",
+    version="1.0.1"
+)
 
-@app.get("/")
+@app.get("/", tags=["Prueba"])
 def main():
-    return {'Hello World from FastAPI':'Alfredo Danel Rolon Salazar'}
+    return {'Hello World from FastAPI':'Danel Rolon Salazar'}
+
+@app.get("/promedio", tags=["Promedio"])
+def promedio():
+    return {'Promedio':(10+8.5+9)/3}
